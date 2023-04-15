05e244b65cd7ddcea4bd2c3cc170cf63cf06cd76
aminul007bd <gem007bd@gmail.com>
2023-04-14 18:10:46 -0700

    add query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,48 @@
-import Head from 'next/head'
-import { Inter } from "@next/font/google";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-const inter = Inter({ subsets: ["latin"] });
+import Head from "next/head";
+
+const POSTS = [
+  { id: "1", title: "Posts1" },
+  { id: "2", title: "Posts2" },
+];
+
+// /posts -> ['posts']
+// /posts/1 -> ['posts', 'post.id]
+// /posts?authorId=1 -> ["posts", {authorId: 1}]
+// posts/2/comments -> ["posts", post.id, 'comments']
 
 export default function Home() {
+  const queryClient = useQueryClient();
+  const postQuery = useQuery({
+    queryKey: ["posts"],
+    // queryFn: () => wait(1000).then(() => [...POSTS]),
+    queryFn: (obj) =>
+      wait(1000).then(() => {
+        // console.log(obj);
+        return [...POSTS];
+      }),
+  });
+
+  const newPostMutation = useMutation({
+    mutationFn: (title: string) => {
+      return wait(1000).then(() =>
+        POSTS.push({ id: crypto.randomUUID(), title })
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
+  });
+
+  if (postQuery.isLoading) return <h1>Loading...</h1>;
+  if (postQuery.isError) return <pre>{JSON.stringify(postQuery.error)}</pre>;
+
+  function wait(duration: number | undefined) {
+    return new Promise((resolve) => setTimeout(resolve, duration));
+  }
+
+  console.log(POSTS);
   return (
     <>
       <Head>
@@ -13,7 +52,18 @@ export default function Home() {
       </Head>
       <main>
         <h1>Tanstack query</h1>
+        <div>
+          {postQuery.data.map((post) => (
+            <div key={post.id}>{post.title}</div>
+          ))}
+        </div>
       </main>
+      <button
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate("new post")}
+      >
+        Add New
+      </button>
     </>
   );
 }
